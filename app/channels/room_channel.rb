0e15753f0b7475037bea1a1118266398b0bbc91f
hemohemo1115 <worldtrigger728@gmail.com>
2019-12-25 20:44:47 -0800

    room idを受け取る

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel"
+    #stream_from "room_channel"
+    stream_from "room_channel_#{params['room']}" 
   end
 
   def unsubscribed
@@ -10,6 +11,8 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     # jsで実行されたspeakのmessageを受け取り、room_channelのreceivedにブロードキャストする,ブロードキャストとは同じネットワーク内にいる全員に同じデータを送ること
     #ActionCable.server.broadcast 'room_channel', message: data['message']
-    Message.create! content: data['message']
+    #Message.create! content: data['message']
+    #メッセージを保存する際にuser情報などと紐づけ
+    Message.create! content: data['message'], user_id: current_user.id, room_id: params['room']
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,36 +1,31 @@
 import consumer from "./consumer"
 
-const chatChannel = consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+$(function() {
+  const chatChannel = consumer.subscriptions.create({ channel: 'RoomChannel', room: $('#messages').data('room_id') }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  // room_channel.rbでブロードキャストされたものがここに届く
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-    //console.log(data);
-    //alert("hoge")
-    return $('#messages').append(data['message']);
-  },
+    received: function(data) {
+      return $('#messages').append(data['message']);
+    },
 
-  // これが実行されるとコンシューマになったRoomChannel#speak({ message: message })が呼ばれる
-  speak: function(message) {
-    
-    //console.log(message);
-    return this.perform('speak', {
-      message: message
-    });
-  },
-});
+    speak: function(message) {
+      return this.perform('speak', {
+        message: message
+      });
+    }
+  });
 
-$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
-  if (event.keyCode === 13) {
-    chatChannel.speak(event.target.value);
-    event.target.value = '';
-    return event.preventDefault();
-  }
+  $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+    if (event.keyCode === 13) {
+      chatChannel.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+  });
 });
