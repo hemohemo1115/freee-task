72793bbc84080701dceed9dad09b35ada7728862
hemohemo1115 <worldtrigger728@gmail.com>
2019-12-25 20:06:46 -0800

    接続ルームのメッセージのみが表示されるようにした

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,11 @@
 class RoomsController < ApplicationController
+  # ついでにRoom一覧を表示させるアクションも追加しておく
+  def index
+    @rooms = Room.all.order(:id)
+  end
+  
   def show
-    # メッセージ一覧を取得
-    @messages = Message.all
+    @room = Room.find(params[:id])
+    @messages = @room.messages
   end
 end
