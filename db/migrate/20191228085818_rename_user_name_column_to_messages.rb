cb25b82aa9a0a7c787f2dcfe0dbbe1272d5df6b9
hemohemo1115 <worldtrigger728@gmail.com>
2019-12-28 03:06:25 -0800

    コメントアウト

--- db/migrate/20191228085818_rename_user_name_column_to_messages.rb
+++ db/migrate/20191228085818_rename_user_name_column_to_messages.rb
@@ -1,5 +1,5 @@
 class RenameUserNameColumnToMessages < ActiveRecord::Migration[6.0]
   def change
-    rename_column :messages, :user_name, :user_id
+    #rename_column :messages, :user_name, :user_id
   end
 end
